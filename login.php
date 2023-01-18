50366905ae83197122407116506dc7e0885c5353
Christopher Cruz <107679454+SircCruz@users.noreply.github.com>
2023-01-18 19:25:13 +0800

    fix register link

--- login.php
+++ login.php
@@ -1,19 +1,27 @@
 <html>
     <head>
         <title>MiniBlog - Login</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"></style>
     </head>
     <body>
         <form method="post">
             <h2>Login</h2>
-            <label>Email: </label>
-            <input type="email" placeholder="Email" id="email" /><br>
-            <label>Password: </label>
-            <input type="password" placeholder="password" id="password" /> <br>
-            <button type="button" onclick="login('login')">Login</button>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="Password">
+            </div>
+            <button type="button" class="btn btn-primary" onclick="login('login')">Login</button>
+            <p>Don't have an account? <a href="register.php">Register</a></p>
         </form>
-        <p>Don't have an account? <a href="2.php">Register</a></p>
-
         <?php require 'script.php' ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
     </body>
     
 </html>
